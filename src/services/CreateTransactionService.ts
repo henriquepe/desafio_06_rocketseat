3fbf4b8ae3c599521f001fbeb9bf41c256f7d35b
Henrique Pires <henrique.p.espindola@gmail.com>
2020-07-09 11:15:04 -0300

    Banco de dados, Models, Rota (PUSH, GET), ServiceC

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,31 @@
 // import AppError from '../errors/AppError';
 
+import { getCustomRepository, TransactionRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
     // TODO
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = transactionsRepository.create({ title, value, type });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
